fdbec49abee2beb01780340516195d627682f63f
Fernando Vieira da Silva <fernandojvdasilva@gmail.com>
2019-05-01 16:56:56 -0300

    Scripts usados durante as aulas

---
+++ scripts.sh
@@ -0,0 +1,15 @@
+
+# Instalando o scrapy
+pip install scrapy
+
+# Inicializa um projeto do scrapy 
+scrapy startproject aula-web
+
+
+# Instalando o pudb3
+pip install pudb
+
+
+# Rodar o Scrapy Shell
+scrapy shell 'https://www.amazon.com.br/games'
+
